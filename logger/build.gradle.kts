148125c8515bb6d26daccd40af49791619cdeea6
arun1810 <arunscs35@gmail.com>
2026-05-14 20:18:15 +0530

    Gradle changes

--- logger/build.gradle.kts
+++ logger/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 }
 
 android {
-    namespace = "com.aetheralstudios.logger"
-    compileSdk = 36
-    version = "1.0.2"
+    namespace = "io.github.arunscs20.logger"
+    compileSdk = 37
+    version = "1.0.1"
 
     defaultConfig {
         minSdk = 24
@@ -27,24 +27,30 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
 
 mavenPublishing{
-    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral()
     signAllPublications()
     coordinates(
         "io.github.arunscs20",
-        "logger",
-        "1.0.0"
+        "logger-runtime",
+        version.toString()
     )
     pom{
         name.set("Logger")
         description.set("A lightweight Android Logcat logger plugin")
+        inceptionYear.set("2026")
         url.set("https://github.com/arunscs20/logger")
-
         licenses{
             license{
                 name.set("The Apache License, Version 2.0")
@@ -65,42 +71,4 @@ mavenPublishing{
             developerConnection.set("scm:git:ssh://git@github.com/arunscs20/logger.git")
         }
     }
-}
-
-//afterEvaluate{
-//    publishing {
-//        publications{
-//            create<MavenPublication>("release"){
-//                from(components["release"])
-//                groupId = "io.github.arunscs20"
-//                artifactId = "logger"
-//                version = "1.0.0"
-//
-//
-//            }
-//        }
-//        repositories{
-//            maven {
-//                name = "MavenCentral"
-//                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-//                credentials {
-//                    username = project.findProperty("mavenCentralUsername") as String?
-//                    password = project.findProperty("mavenCentralPassword") as String?
-//                }
-//            }
-//        }
-//    }
-//    signing {
-//        sign(publishing.publications["release"])
-//    }
-//}
-
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
